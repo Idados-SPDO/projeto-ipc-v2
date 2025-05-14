319cff05cf08a623eca43907189ace0628fecfbb
FGV Idados_SPDO <idados_spdo@fgv.br>
2025-05-14 16:14:05 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ st.set_page_config(
     page_icon="../assets/logo_fgv.png"
 )
 
-st.logo("../assets/logo_ibre.png")
+#st.logo("../assets/logo_ibre.png")
 
 SHEET_NAMES = ["SP", "RS", "RJ", "PE", "MG", "DF", "BA"]
